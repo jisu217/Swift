6b53436adeee9bbac1ce5f8331ed941ca975ee70
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-07-25 16:13:26 +0900

    Update throws.swift

--- practice/throws.swift
+++ practice/throws.swift
@@ -10,11 +10,18 @@ func guessMyName(name input: String) throws {
     
     if input != "홍길동" {
         print("틀렸다")
-        throw MismatchError.nameMismatch
-//      return
+        throw MismatchError.nameMismatch // return
     }
     print("맞췄다")
 }
 
 try guessMyName(name: "사과")
-// 에러 error 처리를 하지 않을 경우 try? 사용
+
+// try?: 에러 처리를 하지 않을 경우 
+// try!: 에러가 무조건 없을 것
+
+do {
+   try guessMyName(name: "이대리")
+} catch {
+    print("잡은 에러: \(error)")
+}
